80c3872fe1891a66aab0224bf8f2e0ed9a466f2d
Arun Kumar R <rarun.unofficial@gmail.com>
2026-04-22 23:38:11 +0530

    Add shape configuration to OCI instance data source

--- oci-instance.tf
+++ oci-instance.tf
@@ -2,6 +2,7 @@ data "oci_core_images" "ubuntu" {
   compartment_id           = var.compartment_id
   operating_system         = "Canonical Ubuntu"
   operating_system_version = "24.04"
+  shape = "VM.Standard3.Flex"
 
   sort_by    = "TIMECREATED"
   sort_order = "DESC"
