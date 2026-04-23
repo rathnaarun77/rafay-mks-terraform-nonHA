a3ae3b349fedddbe4cef763985d4aa2c8b7563c4
Arun <arun@Rafays-MacBook-Pro.local>
2026-04-23 12:39:29 +0530

    update output.tf to add ssh command

--- output.tf
+++ output.tf
@@ -2,12 +2,8 @@ output "oci_node_name"{
     value = oci_core_instance.node1.display_name
 }
 
-output "oci_node_public_IP" {
-    value = oci_core_instance.node1.public_ip
-}
-
-output "oci_node_private_IP" {
-    value = oci_core_instance.node1.private_ip
+output "ssh_oci_node" {
+  value = "ssh ubuntu@${oci_core_instance.node1.public_ip}"
 }
 
 output "mks_cluster_name" {
