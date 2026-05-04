d0ae5016faf781dc8d3395ba0049526b6f6c8033
Arun <rarun.unofficial@gmail.com>
2026-05-04 12:12:51 +0530

    adding dependency for cluster kubeconfig download

--- mks-cluster.tf
+++ mks-cluster.tf
@@ -52,9 +52,13 @@ resource "rafay_mks_cluster" "mks-cluster" {
         }
       }
     }
+
 # get kubeconfig for a cluster
 data "rafay_download_kubeconfig" "kubeconfig_cluster" {
   cluster = "${var.prefix_name}-${local.random_name}"
-}
 
+  depends_on = [
+    rafay_cluster.cluster
+  ]
+}
 
