dec550e3537bed95332f98745375580e87a67efa
Arun <rarun.unofficial@gmail.com>
2026-05-11 21:45:09 +0530

    edit mks-cluster file to support multi-node

--- mks-cluster.tf
+++ mks-cluster.tf
@@ -6,55 +6,74 @@ resource "local_file" "ssh_key" {
 resource "rafay_mks_cluster" "mks-cluster" {
   api_version = "infra.k8smgmt.io/v3"
   kind        = "Cluster"
+
   metadata = {
     name    = "${var.prefix_name}-${local.random_name}"
     project = var.project
   }
+
   spec = {
     blueprint = {
       name = var.blueprint
     }
+
     config = {
-      auto_approve_nodes      = true
-      kubernetes_version      = var.k8s_ver
-      installer_ttl           = 365
-      platform_version        = var.platform_ver
+      auto_approve_nodes = true
+      kubernetes_version = var.k8s_ver
+      installer_ttl      = 365
+      platform_version   = var.platform_ver
+
       network = {
         cni = {
           name    = var.cni
           version = var.cni_ver
         }
+
         pod_subnet     = var.pod_subnet
         service_subnet = var.service_subnet
       }
+
       cluster_ssh = {
         username         = "ubuntu"
         port             = "22"
         private_key_path = local_file.ssh_key.filename
       }
+
       nodes = {
-        (oci_core_instance.node1.display_name) = {
+        for idx, instance in oci_core_instance.nodes :
+        instance.display_name => {
           arch             = "amd64"
-          hostname         = oci_core_instance.node1.display_name
+          hostname         = instance.display_name
           operating_system = var.mks_operating_system
-          private_ip       = oci_core_instance.node1.private_ip
+          private_ip       = instance.private_ip
 
           roles = concat(
-            ["ControlPlane", "Worker"],
+            (
+              var.dedicated_master ?
+              (
+                idx < var.cp_count ?
+                ["ControlPlane"] :
+                ["Worker"]
+              ) :
+              (
+                idx < var.cp_count ?
+                ["ControlPlane", "Worker"] :
+                ["Worker"]
+              )
+            ),
             var.block_vol_size > 0 ? ["Storage"] : []
           )
 
           ssh = {
-            ip_address = oci_core_instance.node1.public_ip
+            ip_address = instance.public_ip
           }
         }
       }
-        }
-      }
     }
+  }
+}
 
 # get kubeconfig for a cluster
 data "rafay_download_kubeconfig" "kubeconfig_cluster" {
   cluster = rafay_mks_cluster.mks-cluster.metadata.name
 }
-
