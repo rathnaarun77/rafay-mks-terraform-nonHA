cd7d77e873776ea98b178cee1d4ba52630de2e5f
Arun <rarun.unofficial@gmail.com>
2026-05-11 21:50:09 +0530

    updating output.tf

--- output.tf
+++ output.tf
@@ -1,23 +1,58 @@
-output "oci_node_name"{
-    value = oci_core_instance.node1.display_name
+output "oci_node_names" {
+  description = "Display names of all OCI nodes"
+
+  value = [
+    for instance in oci_core_instance.nodes :
+    instance.display_name
+  ]
 }
 
-output "ssh_oci_node" {
-  value = "ssh ubuntu@${oci_core_instance.node1.public_ip}"
+output "ssh_oci_nodes" {
+  description = "SSH commands for all OCI nodes"
+
+  value = [
+    for instance in oci_core_instance.nodes :
+    "ssh ubuntu@${instance.public_ip}"
+  ]
 }
 
 output "mks_cluster_name" {
-    value = rafay_mks_cluster.mks-cluster.metadata.name
-  
+  description = "Name of the MKS cluster"
+
+  value = rafay_mks_cluster.mks-cluster.metadata.name
 }
 
 output "kubeconfig_cluster" {
-  description = "kubeconfig_cluster"
-  value       = data.rafay_download_kubeconfig.kubeconfig_cluster.kubeconfig
+  description = "Kubeconfig for the cluster"
+
+  value = data.rafay_download_kubeconfig.kubeconfig_cluster.kubeconfig
+
+  sensitive = true
 }
 
-output "oci_instance_id" {
-  description = "oci id of the instance"
-  value       = "${oci_core_instance.node1.id}"
+output "oci_instance_ids" {
+  description = "OCI instance IDs"
+
+  value = [
+    for instance in oci_core_instance.nodes :
+    instance.id
+  ]
 }
 
+output "oci_private_ips" {
+  description = "Private IPs of OCI nodes"
+
+  value = [
+    for instance in oci_core_instance.nodes :
+    instance.private_ip
+  ]
+}
+
+output "oci_public_ips" {
+  description = "Public IPs of OCI nodes"
+
+  value = [
+    for instance in oci_core_instance.nodes :
+    instance.public_ip
+  ]
+}
